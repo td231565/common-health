fbea766a5afc93e6351f8720d70e40ece7b59109
Richard Chen <td231565@gmail.com>
2023-11-29 16:17:29 +0800

    fix: layout shift after image loaded

--- src/components/header/HeaderBanner.vue
+++ src/components/header/HeaderBanner.vue
@@ -1,17 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isBannerLoaded = ref(false)
+</script>
 
 <template>
-  <div class="header-banner">
+  <div class="header-banner" :class="{ default: !isBannerLoaded }">
     <picture>
       <source
-        media="(max-width: 375px)"
+        media="(max-width: 480px)"
         srcset="https://picsum.photos/300/250"
       />
       <source
-        media="(min-width: 465px)"
+        media="(min-width: 481px)"
         srcset="https://picsum.photos/970/250"
       />
-      <img src="https://picsum.photos/300/250" alt="header-banner" />
+      <img
+        src="https://picsum.photos/300/250"
+        alt="header-banner"
+        @load="isBannerLoaded = true"
+      />
     </picture>
   </div>
 </template>
@@ -21,5 +29,16 @@
   padding: 12px 0;
   display: flex;
   justify-content: center;
+  &.default {
+    width: 970px;
+    height: 250px;
+  }
+}
+
+@media screen and (max-width: 480px) {
+  .header-banner.default {
+    width: 300px;
+    height: 250px;
+  }
 }
 </style>
